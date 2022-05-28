bdb7e315c62ea3f498e32b381cfcf09cfe2f9645
elena_kau <lelickau@yandex.ru>
2022-05-28 22:36:50 +0200

    created helpers validate, added validation error output

--- client/src/pages/detailsPage/DetailsPage.tsx
+++ client/src/pages/detailsPage/DetailsPage.tsx
@@ -1,7 +1,8 @@
-import React, { FC, ChangeEvent, MouseEvent, useEffect, useState } from 'react'
+import React, { FC, ChangeEvent, FocusEvent, MouseEvent, useEffect, useState } from 'react'
 import { useAppDispatch, useAppSelector } from 'hooks/reduxHooks'
 import { useNavigate, useParams } from 'react-router-dom'
 import { updateUserData } from 'store/slices/userSlice'
+import { validate } from 'helpers/validate'
 import ButtonElem from 'components/UI/buttonElem/ButtonElem'
 import InputElem from 'components/UI/inputElem/InputElem'
 import SelectElem from 'components/UI/selectElem/SelectElem'
@@ -23,6 +24,18 @@ const DetailsPage: FC = () => {
         country: user?.country,
         id: user?.id,
     })
+
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        age: '',
+    })
+
+    const blurHandler = (e: FocusEvent<HTMLInputElement>) => {
+        const isError = validate(e.target.name, e.target.value)
+        setErrors({...errors, [e.target.name]: isError})
+    }
 
     useEffect(() => {
         if (user) {
@@ -52,39 +65,43 @@ const DetailsPage: FC = () => {
         <form className="details__form">
             <h1 className="details__title">Update contact</h1>
             <label className="details__label">
-                Name
+                Name <span className="error">{errors.name ? '*' + errors.name : ''}</span>
                 <InputElem
                     name="name"
                     type="text"
                     value={form.name}
                     onChange={changeHandler}
+                    onBlur={blurHandler}
                 />
             </label>
             <label className="details__label">
-                Email
+                Email <span className="error">{errors.email ? '*' + errors.email : ''}</span>
                 <InputElem
                     name="email"
                     type="email"
                     value={form.email}
                     onChange={changeHandler}
+                    onBlur={blurHandler}
                 />
             </label>
             <label className="details__label">
-                Phone
+                Phone <span className="error">{errors.phone ? '*' + errors.phone : ''}</span>
                 <InputElem
                     name="phone"
                     type="text"
                     value={form.phone}
                     onChange={changeHandler}
+                    onBlur={blurHandler}
                 />
             </label>
             <label className="details__label">
-                Age
+                Age <span className="error">{errors.age ? '*' + errors.age : ''}</span>
                 <InputElem
                     name="age"
                     type="number"
                     value={form.age}
                     onChange={changeHandler}
+                    onBlur={blurHandler}
                 />
             </label>
             <SelectElem
