5786389c08679f2f25d4cee816cefbd8a0c040c8
elena_kau <lelickau@yandex.ru>
2022-05-28 23:07:13 +0200

    created SelectElem

--- client/src/pages/mainPage/MainPage.tsx
+++ client/src/pages/mainPage/MainPage.tsx
@@ -1,14 +1,24 @@
 import React, { FC } from 'react'
 import { useAppSelector } from 'hooks/reduxHooks'
 import UserItem from 'components/userItem/UserItem'
+import SelectElem from 'components/UI/selectElem/SelectElem'
 
 import './mainPage.scss'
 
 const MainPage: FC = () => {
     const {users} = useAppSelector(state => state.users)
 
+    const optionsData = [
+        {val: 'byAll', text: 'all'},
+        {val: 'byName', text: 'by name'},
+    ]
+
     return (
         <section className="container users">
+            <SelectElem
+                optionsData={optionsData}
+                name="sortUsers"
+            />
             <div className="users__content">
                 {
                     users.map(user => <UserItem key={user.id} userData={user}/>)
