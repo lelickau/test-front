44ecf0ac50e914c8640f1ed603afd47765b637a0
elena_kau <lelickau@yandex.ru>
2022-05-28 22:54:14 +0200

    created acync thunk getUserData

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useAppDispatch } from 'hooks/reduxHooks'
+import { Route, Routes } from 'react-router-dom'
+import { getUsersData } from 'store/slices/userSlice'
+import MainPage from 'pages/mainPage/MainPage'
 
 function App() {
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(getUsersData())
+    }, [dispatch])
+
 
     return (
         <>
         <Routes>
-
+            <Route path="/" element={<MainPage/>}></Route>
         </Routes>
         </>
     )
